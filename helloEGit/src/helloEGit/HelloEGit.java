943bef26ab73b287c91be7251cfd18594e06b41c
sgju <jsg6780@gmail.com>
2017-04-12 11:13:08 +0900

    Add something

--- helloEGit/src/helloEGit/HelloEGit.java
+++ helloEGit/src/helloEGit/HelloEGit.java
@@ -2,6 +2,8 @@ package helloEGit;
 
 public class HelloEGit {
 	public static void main(String[] args){
+		System.out.println("feature 브랜치에서 작성한 내용 - 추후 Merge");
 		System.out.println("Hello, EGit!");
+		System.out.println("feature 브랜치에서 작성한 내용 - 추후 Merge");
 	}
 }
